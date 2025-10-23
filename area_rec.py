126a9486d2eded8b74bae94fd6c9c8a2a111919a
aeviviana0323-lgt <aeviviana0323@gmail.com>
2025-10-23 18:14:51 -0500

    En este avance se calcula el área del rectangulo

---
+++ area_rec.py
@@ -0,0 +1,10 @@
+base = float(input("Escriba la base del rectángulo: "))
+
+# Pedir luego la altura
+altura = float(input("Escriba la altura del rectángulo: "))
+
+# Calcular el área
+area = base * altura
+
+# Mostrar el resultado
+print("El área del rectángulo es:",area)
